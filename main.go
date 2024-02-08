ac4fd8378df3ec4ff1283c77bcbddfd94a8be634
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-02-08 14:44:46 -0500

    Added function to get cpuinfo

--- main.go
+++ main.go
@@ -2,41 +2,104 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os/user"
 	"runtime"
-
-	"golang.org/x/sys/windows"
+	"strings"
 
 	osinfo "github.com/JZXHanta/OSInfo"
+	// "github.com/mohae/joefriday/cpu/cpuinfo"
 	"github.com/pbnjay/memory"
+	"github.com/shirou/gopsutil/v3/cpu"
 )
 
-func moreInfo() {
+func cpuInfo() string {
+	var c string
+	switch runtime.GOOS {
+	case "windows":
+		cpuinfo, err := cpu.Info()
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		//fmt.Print(cpuinfo)
+		c = cpuinfo[0].ModelName
+
+	case "linux":
+		cpuinfo, err := cpu.Info()
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		//fmt.Print(cpuinfo)
+		c = cpuinfo[0].ModelName
+
+	}
+	return fmt.Sprintf("CPU     : %s", c)
+}
+
+func osInfo() (string, string) {
+	var os string
+	var ver string
 	switch runtime.GOOS {
 	case "linux":
-		fmt.Println("OS      : ", osinfo.LinuxDistro())
-		fmt.Println("Version : ", osinfo.LinuxVersion())
+		os = fmt.Sprint("OS      : ", osinfo.LinuxDistro())
+		ver = fmt.Sprint("Version : ", osinfo.LinuxVersion())
 	case "windows":
-		fmt.Println("OS      : ", osinfo.WindowsOS())
-		fmt.Println("Version : ", osinfo.WindowsKernel())
+		os = fmt.Sprint("OS      : ", osinfo.WindowsOS())
+		ver = fmt.Sprint("Version : ", osinfo.WindowsKernel())
 	}
+	return os, ver
 }
 
 func totalMemory() string {
 	total := ((memory.TotalMemory() / 1024) / 1024) / 1000
 	free := ((memory.FreeMemory() / 1024) / 1024) / 1000
 	used := total - free
-	str := fmt.Sprintf("Ram     :  %d / %d GiB", used, total)
-	return str
+	return fmt.Sprintf("Ram     : %d / %d GiB", used, total)
+}
+
+func userNameAndHostName() string {
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	u := currentUser.Username
+	userAndComp := strings.Split(u, "\\")
+	userName := userAndComp[1]
+	compName := userAndComp[0]
+
+	return fmt.Sprintf("%s @ %s", userName, compName)
+}
+
+func goFetch() {
+	OS, VER := osInfo()
+	USERATHOST := userNameAndHostName()
+	SEP := "------------------------"
+	MEMUSED := totalMemory()
+	CPU := cpuInfo()
+
+	fmt.Println("")
+	fmt.Println(USERATHOST) // "User @ Hostname"
+	fmt.Println(SEP)        // "-----------------------"
+	fmt.Println(OS)         // "OS      :  Microsoft Windows 11 Pro"
+	fmt.Println(VER)        // "Version :  22621"
+	fmt.Println(MEMUSED)
+	// "Ram     :  9 / 32 GiB"
+	// TODO: (below)
+	// Add TOML config file?
+	// fmt.Println(UPTIME)
+	// fmt.Println(PACKAGES)
+	// fmt.Println(SHELL)
+	// fmt.Println(RESOLUTION)
+	// fmt.Println(TERMINAL)
+	// fmt.Println(CPU)
+	fmt.Println(CPU) // "CPU     : AMD Ryzen 5 3600 6-Core Processor"
+	// fmt.Println(GPU)
+	// fmt.Println(DISKUSED)
+	fmt.Println("")
+	// maj, min, build := windows.RtlGetNtVersionNumbers()
+	// fmt.Println(maj, min, build)
 }
 
 func main() {
-	// fmt.Println(getPlatform())
-	// fmt.Println(get_arch())
-	// readLinuxReleaseInfo()
-	moreInfo()
-	fmt.Println(totalMemory())
-	computerName, _ := windows.ComputerName()
-	fmt.Println(computerName)
-	maj, min, build := windows.RtlGetNtVersionNumbers()
-	fmt.Println(maj, min, build)
+	goFetch()
 }
