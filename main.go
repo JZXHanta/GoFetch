500e02b59f367abe8baf838fb736b4c4300d808a
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-02-08 23:07:29 -0500

    update username and hostname functions

--- main.go
+++ main.go
@@ -68,35 +68,28 @@ func totalMemory() string {
 }
 
 // TODO: Does not work properly in linux, fails with index error on line 76
-// func userNameAndHostName() string {
-// 	var user string
-// 	var host string
-// 	user, hostname
-// 	switch runtime.GOOS {
-// 	case "linux":
-
-// 	}
-// 	currentUser, err := user.Current()
-// 	if err != nil {
-// 		log.Fatalf(err.Error())
-// 	}
-// 	u := currentUser.Username
-// 	userAndComp := strings.Split(u, "\\")
-// 	userName := userAndComp[1]
-// 	compName := userAndComp[0]
-
-// 	return fmt.Sprintf("%s @ %s", userName, compName)
-// }
+func userNameAndHostName() string {
+	var str string
+	switch runtime.GOOS {
+	case "linux":
+		str = userNameLinux()
+	case "windows":
+		str = userNameWindows()
+	}
+	return str
+}
 
 func userNameWindows() string {
+	var userName string
+	var compName string
 	currentUser, err := user.Current()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	u := currentUser.Username
 	userAndComp := strings.Split(u, "\\")
-	userName := userAndComp[1]
-	compName := userAndComp[0]
+	userName = userAndComp[1]
+	compName = userAndComp[0]
 
 	return fmt.Sprintf("%s @ %s", userName, compName)
 }
@@ -181,7 +174,7 @@ func shell() string {
 
 func goFetch() {
 	OS, VER := osInfo()
-	USERATHOST := userNameWindows()
+	USERATHOST := userNameAndHostName()
 	SEP := "------------------------"
 	MEMUSED := totalMemory()
 	CPU := cpuInfo()
@@ -190,15 +183,15 @@ func goFetch() {
 	SHELL := shell()
 
 	fmt.Println("")
-	fmt.Println(USERATHOST, userNameLinux()) // "User @ Hostname"
-	fmt.Println(SEP)                         // "-----------------------"
-	fmt.Println(OS)                          // "OS      :  Microsoft Windows 11 Pro"
-	fmt.Println(VER)                         // "Version :  22621"
-	fmt.Println(MEMUSED)                     // "Ram     :  9 / 32 GiB"
-	fmt.Println(UPTIME)                      // "Uptime  :  43h45m45.64s"
-	fmt.Println(CPU)                         // "CPU     :  AMD Ryzen 5 3600 6-Core Processor"
-	fmt.Println(PACKAGES)                    // "Packages: (Choco: 19)"
-	fmt.Println(SHELL)                       // "Shell   : pwsh"
+	fmt.Println(USERATHOST) // "User @ Hostname"
+	fmt.Println(SEP)        // "-----------------------"
+	fmt.Println(OS)         // "OS      :  Microsoft Windows 11 Pro"
+	fmt.Println(VER)        // "Version :  22621"
+	fmt.Println(MEMUSED)    // "Ram     :  9 / 32 GiB"
+	fmt.Println(UPTIME)     // "Uptime  :  43h45m45.64s"
+	fmt.Println(CPU)        // "CPU     :  AMD Ryzen 5 3600 6-Core Processor"
+	fmt.Println(PACKAGES)   // "Packages: (Choco: 19)"
+	fmt.Println(SHELL)      // "Shell   : pwsh"
 	fmt.Println("")
 	// TODO: (below)
 	// Add TOML config file?
