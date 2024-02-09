95d0b1aebaac819edf3ad41dcffaad8a446f01fa
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-02-08 22:08:54 -0500

    Added functions for packageCount and shell

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
 	"os/user"
 	"runtime"
+	"strconv"
 	"strings"
 
 	osinfo "github.com/JZXHanta/OSInfo"
-	// "github.com/mohae/joefriday/cpu/cpuinfo"
 	"github.com/mackerelio/go-osstat/uptime"
 	"github.com/pbnjay/memory"
 	"github.com/shirou/gopsutil/v3/cpu"
@@ -78,6 +79,73 @@ func userNameAndHostName() string {
 	return fmt.Sprintf("%s @ %s", userName, compName)
 }
 
+func chocoPackages() (string, error) {
+	out, err := exec.Command("powershell", "-NoProfile", "-NonInteractive", "(ls C:\\ProgramData\\chocolatey\\lib).Count").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := fmt.Sprint(string(out))
+	count, err := strconv.ParseInt(strings.TrimSpace(c), 10, 10)
+	if err != nil {
+		log.Fatalf(err.Error())
+		return "", err
+	}
+
+	return fmt.Sprintf("(Choco: %d)", count), nil
+}
+
+func wingetPackages() (string, error) {
+	out, err := exec.Command("powershell", "-NoProfile", "-NonInteractive", "(winget list).Count").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := fmt.Sprint(string(out))
+	count, err := strconv.ParseInt(strings.TrimSpace(c), 10, 10)
+	if err != nil {
+		log.Fatalf(err.Error())
+		return "", err
+	}
+
+	return fmt.Sprintf("(Winget: %d)", count), nil
+}
+
+func packageCount() string {
+	var packageCount []string
+	var str string
+	chocoCount, chocoErr := chocoPackages()
+	if chocoErr != nil {
+		packageCount = append(packageCount, chocoCount)
+	}
+	wingetCount, wingetErr := wingetPackages()
+	if wingetErr != nil {
+		packageCount = append(packageCount, wingetCount)
+	}
+	packageCount = append(packageCount, chocoCount)
+	if len(packageCount) > 0 {
+		str = strings.Join(packageCount, ", ")
+	} else {
+		str = packageCount[0]
+	}
+
+	return fmt.Sprintf("Packages: %s", str)
+}
+
+func shell() string {
+	var shell string
+	switch runtime.GOOS {
+	case "windows":
+		shell = "pwsh"
+	case "linux":
+		out, err := exec.Command("echo $0").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := fmt.Sprint(string(out))
+		shell = strings.Replace(s, "-", "", 0)
+	}
+	return fmt.Sprintf("Shell   : %s", shell)
+}
+
 func goFetch() {
 	OS, VER := osInfo()
 	USERATHOST := userNameAndHostName()
@@ -85,6 +153,8 @@ func goFetch() {
 	MEMUSED := totalMemory()
 	CPU := cpuInfo()
 	UPTIME := upTime()
+	PACKAGES := packageCount()
+	SHELL := shell()
 
 	fmt.Println("")
 	fmt.Println(USERATHOST) // "User @ Hostname"
@@ -92,17 +162,17 @@ func goFetch() {
 	fmt.Println(OS)         // "OS      :  Microsoft Windows 11 Pro"
 	fmt.Println(VER)        // "Version :  22621"
 	fmt.Println(MEMUSED)    // "Ram     :  9 / 32 GiB"
+	fmt.Println(UPTIME)     // "Uptime  :  43h45m45.64s"
+	fmt.Println(CPU)        // "CPU     :  AMD Ryzen 5 3600 6-Core Processor"
+	fmt.Println(PACKAGES)   // "Packages: (Choco: 19)"
+	fmt.Println(SHELL)      // "Shell   : pwsh"
+	fmt.Println("")
 	// TODO: (below)
 	// Add TOML config file?
-	fmt.Println(UPTIME)
-	// fmt.Println(PACKAGES)
-	// fmt.Println(SHELL)
 	// fmt.Println(RESOLUTION)
 	// fmt.Println(TERMINAL)
-	fmt.Println(CPU) // "CPU     : AMD Ryzen 5 3600 6-Core Processor"
 	// fmt.Println(GPU)
 	// fmt.Println(DISKUSED)
-	fmt.Println("")
 	// maj, min, build := windows.RtlGetNtVersionNumbers()
 	// fmt.Println(maj, min, build)
 }
